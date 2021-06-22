0952b48e2d9f4eb1bf62f3bdf62ed6d3029193b4
kunal121212 <86285325+kunal121212@users.noreply.github.com>
2021-06-22 18:00:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ if HEROKU:
     from dotenv import load_dotenv
     
     load_dotenv()  # take environment variables from .env.
-    API_ID = int(environ["API_ID"])
-    API_HASH = environ["API_HASH"]
-    SESSION_STRING = environ["SESSION_STRING"]  # Check Readme for session
-    ARQ_API_KEY = environ["ARQ_API_KEY"]
-    DEFAULT_SERVICE = environ["DEFAULT_SERVICE"] if "DEFAULT_SERVICE" in environ else "youtube"
+    API_ID = int(environ["4195152"])
+    API_HASH = environ["2642c0b549ceca3a381c26339e0017e0"]
+    SESSION_STRING = environ["BQAmR7AWGuChz_k-51vUR1J3JJPf-cBCNp3BPaFeE4b_zk527dB-FfH6iJ2q7AOKVcc007L1o5KhgMuiHdptm9jeLZNrdbS-4BJab1ssVy_rW_DyBOv74a7nXJwiDW35EfMF-7KFcfmAgckTpRb5XZ5Sa4xhb6QWBhf255JHtf_EEVE_6ZDCWZyIKYh9xxNCef1gkovx0-X5_jfumlfb3yUL8jsccjzKyH1qoCCf8sfsA5gFqm-oEEzKdvD4g01M92u0J5VWPEIGen8JiHkGC16Amjbp3JZyAcJqa19LH5mEURy1CAVPigit2qQ1W7Iqrq88Ver9WEWAvysrv0C4xUARZ1sg1AA"]  # Check Readme for session
+    ARQ_API_KEY = environ["XGNFME-JZYRAG-SXNFIX-MKKXKA-ARQ"]
+    DEFAULT_SERVICE = environ["youtube"] if "DEFAULT_SERVICE" in environ else "youtube"
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
